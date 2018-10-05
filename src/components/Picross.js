706e868e022dc82d3d554201b6f18b5e0cb3b748
mijara <marjara35@gmail.com>
2018-10-05 13:06:54 -0300

    Fix styling

--- src/components/Picross.js
+++ src/components/Picross.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const cellSize = 30
-const numberSize = 30
+const cellSize = 25
+const numberSize = 25
 
 class Picross extends React.Component {
   state = {
@@ -13,11 +13,13 @@ class Picross extends React.Component {
     rowsSize: 0,
     cursorX: 0,
     cursorY: 0,
+    markHeldState: 0,
+    discardHeldState: 0,
   }
 
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyDown)
-    // TODO: document.addEventListener("keyup", this.handleKeyUp)
+    document.addEventListener("keyup", this.handleKeyUp)
 
     const rowsNumbers = this.calculateRowsNumbers()
     const colsNumbers = this.calculateColsNumbers()
@@ -42,6 +44,8 @@ class Picross extends React.Component {
         {this.renderRowsNumbers()}
         {this.renderColsNumbers()}
         {this.state.data.map((row, y) => this.renderRow(y, row))}
+        {this.renderDividers()}
+        {this.renderCursor()}
       </svg>
     )
   }
@@ -51,9 +55,9 @@ class Picross extends React.Component {
       return row.map((number, i) =>
         <text key={`row-${rowIndex}-${i}`}
               fontSize={numberSize}
+              fill={this.state.cursorY === rowIndex ? '#2c71f7' : 'black'}
               x={(this.state.rowsSize - (row.length - i)) * numberSize}
-              y={this.state.marginTop + rowIndex * cellSize + (cellSize - 20) / 2}
-              fill="black">
+              y={this.state.marginTop + rowIndex * cellSize + (cellSize - 20) / 2}>
           {number}
         </text>
       )
@@ -65,9 +69,9 @@ class Picross extends React.Component {
       return col.map((number, i) =>
         <text key={`col-${colIndex}-${i}`}
               fontSize={numberSize}
+              fill={this.state.cursorX === colIndex ? '#2c71f7' : 'black'}
               y={(this.state.colsSize - (col.length - i)) * numberSize}
-              x={this.state.marginLeft + colIndex * cellSize + (cellSize - 20) / 2}
-              fill="black">
+              x={this.state.marginLeft + colIndex * cellSize + (cellSize - 20) / 2}>
           {number}
         </text>
       )
@@ -78,16 +82,52 @@ class Picross extends React.Component {
     return row.map((cell, x) =>
       <rect
         key={y * this.state.data.length + x}
-        style={{
-          stroke: this.isCursorOn(x, y) ? 'white' : 'black',
-          strokeWidth: this.isCursorOn(x, y) ? 2 : 1,
-        }}
+        style={{ stroke: 'black', strokeWidth: 1 }}
         onClick={() => this.toggleCell(x, y)}
         onContextMenu={(e) => {e.preventDefault(); this.discardCell(x, y)}}
         y={this.state.marginTop + y * cellSize} x={this.state.marginLeft + x * cellSize}
         width={cellSize} height={cellSize}
         fill={getColor(this.state.data[y][x])} />
     )
+  }
+
+  renderCursor = () => {
+    return <rect
+      style={{ stroke: 'black', strokeWidth: 3 }}
+      y={this.state.marginTop + this.state.cursorY * cellSize}
+      x={this.state.marginLeft + this.state.cursorX * cellSize}
+      width={cellSize} height={cellSize}
+      fill="#FFFFFF00"/>
+  }
+
+  renderDividers = () => {
+    let dividers = []
+
+    let width = this.state.data[0].length
+    let height = this.state.data.length
+
+    let howManyVerticalDividers = Math.floor(width / 5)
+    let howManyHorizontalDividers = Math.floor(height / 5)
+
+    let style = { stroke: 'black', strokeWidth: 2 }
+
+    for (let i = 1; i < howManyVerticalDividers + 1; i++) {
+      dividers.push(<line
+        key={`line-vertical-${i}`}
+        style={style}
+        x1={i * cellSize * 5 + this.state.marginLeft} x2={i * cellSize * 5 + this.state.marginLeft}
+        y1={this.state.marginTop} y2={height * cellSize + this.state.marginTop}/>)
+    }
+
+    for (let i = 1; i < howManyHorizontalDividers + 1; i++) {
+      dividers.push(<line
+        key={`line-horizontal-${i}`}
+        style={style}
+        x1={this.state.marginLeft} x2={width * cellSize + this.state.marginLeft}
+        y1={i * cellSize * 5 + this.state.marginTop} y2={i * cellSize * 5 + this.state.marginTop}/>)
+    }
+
+    return dividers
   }
 
   handleKeyDown = (e) => {
@@ -100,8 +140,31 @@ class Picross extends React.Component {
 
     switch (e.keyCode) {
       case 90:
-        return this.toggleCell(this.state.cursorX, this.state.cursorY)
+        if (this.getCurrentCell() === 1) {
+          this.setState({
+            markHeldState: 2, // turning off.
+          })
+
+        } else if (this.getCurrentCell() === 0) {
+          this.setState({
+            markHeldState: 1, // turning on.
+          })
+        }
+
+        this.toggleCell(this.state.cursorX, this.state.cursorY)
+
+        break
       case 88:
+        if (this.getCurrentCell() === 2) {
+          this.setState({
+            discardHeldState: 2, // turning off.
+          })
+        } else if (this.getCurrentCell() === 0) {
+          this.setState({
+            discardHeldState: 1, // turning on.
+          })
+        }
+
         return this.discardCell(this.state.cursorX, this.state.cursorY)
       case 39:
         return this.moveCursor(1, 0)
@@ -111,16 +174,45 @@ class Picross extends React.Component {
         return this.moveCursor(0, 1)
       case 38:
         return this.moveCursor(0, -1)
-      default:
-        break
+      case 69:
+        return this.exportDrawing()
+    }
+  }
+
+  handleKeyUp = (e) => {
+    switch (e.keyCode) {
+      case 90:
+        return this.setState({
+          markHeldState: 0,
+        })
+      case 88:
+        return this.setState({
+          discardHeldState: 0,
+        })
     }
   }
 
   moveCursor = (dx, dy) => {
-    this.setState({
-      cursorX: mod(this.state.cursorX + dx, this.state.data[0].length),
-      cursorY: mod(this.state.cursorY + dy, this.state.data.length),
-    })
+    let cursorX = mod(this.state.cursorX + dx, this.state.data[0].length)
+    let cursorY = mod(this.state.cursorY + dy, this.state.data.length)
+
+    if (this.state.markHeldState === 1) {
+      if (this.getCell(cursorX, cursorY) !== 2) {
+        this.setCell(cursorX, cursorY, 1)
+      }
+    } else if (this.state.markHeldState === 2) {
+      if (this.getCell(cursorX, cursorY) !== 2) {
+        this.setCell(cursorX, cursorY, 0)
+      }
+    } else if (this.state.discardHeldState === 1) {
+      if (this.getCell(cursorX, cursorY) !== 1) {
+        this.discardCell(cursorX, cursorY)
+      }
+    } else if (this.state.discardHeldState === 2) {
+      this.setCell(cursorX, cursorY, 0)
+    }
+
+    this.setState({ cursorX, cursorY })
   }
 
   discardCell = (x, y) => {
@@ -135,6 +227,15 @@ class Picross extends React.Component {
   toggleCell = (x, y) => {
     let data = this.copyData(this.state.data)
     data[y][x] = data[y][x] > 0 ? 0 : 1
+
+    this.setState({
+      data,
+    })
+  }
+
+  setCell = (x, y, val) => {
+    let data = this.copyData(this.state.data)
+    data[y][x] = val
 
     this.setState({
       data
@@ -228,6 +329,18 @@ class Picross extends React.Component {
   isCursorOn = (x, y) => {
     return this.state.cursorX === x && this.state.cursorY === y
   }
+
+  getCurrentCell = () => {
+    return this.getCell(this.state.cursorX, this.state.cursorY)
+  }
+
+  getCell = (x, y) => {
+    return this.state.data[y][x]
+  }
+
+  exportDrawing = () => {
+    console.log(this.state.data)
+  }
 }
 
 const mod = (x, y) => {
@@ -238,9 +351,9 @@ const getColor = (value) => {
   if (value === 1) {
     return '#BBC42A'
   } else if (value === 2) {
-    return '#06101d'
+    return '#535353'
   } else {
-    return '#ED6E46'
+    return '#F7F7F7'
   }
 }
 
